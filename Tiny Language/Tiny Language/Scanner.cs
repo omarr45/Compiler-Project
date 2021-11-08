f60b5e3ed00161e9bf99d3b811807456ac45d6f3
Shehab <shehabihab@hotmail.com>
2021-11-08 13:21:12 +0200

    added Tokens, ReservedWords and Operators, implemented isIdentifier & isConstant Functions

--- Tiny Language/Tiny Language/Scanner.cs
+++ Tiny Language/Tiny Language/Scanner.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 public enum Token_Class
 {
-    If, PlusOp
+    If, Int , Float , String , Read , Write , Repeat , Until , Elseif , Else , Then , Return , Endl,
+
+    Semicolon, Comma, LParanthesis, RParanthesis, EqualOp, LessThanOp, GreaterThanOp, NotEqualOp, AssignmentOp,
+    PlusOp, MinusOp, MultiplyOp, DivideOp,
+    Idenifier, Constant
+    //,Dot
     // TODO: Add Tokens
 }
 namespace Tiny_Language
@@ -25,8 +31,33 @@ namespace Tiny_Language
         public Scanner()
         {
             ReservedWords.Add("if", Token_Class.If);
+            ReservedWords.Add("int", Token_Class.Int);
+            ReservedWords.Add("float", Token_Class.Float);
+            ReservedWords.Add("string", Token_Class.String);
+            ReservedWords.Add("read", Token_Class.Read);
+            ReservedWords.Add("write", Token_Class.Write);
+            ReservedWords.Add("repeat", Token_Class.Repeat);
+            ReservedWords.Add("until", Token_Class.Until);
+            ReservedWords.Add("elseif", Token_Class.Elseif);
+            ReservedWords.Add("else", Token_Class.Else);
+            ReservedWords.Add("then", Token_Class.Then);
+            ReservedWords.Add("return", Token_Class.Return);
+            ReservedWords.Add("endl", Token_Class.Endl);
             // TODO: Add Reserved Words
+            Operators.Add(";", Token_Class.Semicolon);
+            Operators.Add(",", Token_Class.Comma);
+            Operators.Add("(", Token_Class.LParanthesis);
+            Operators.Add(")", Token_Class.RParanthesis);
+            Operators.Add("=", Token_Class.EqualOp);
+            Operators.Add("<", Token_Class.LessThanOp);
+            Operators.Add(">", Token_Class.GreaterThanOp);
+            Operators.Add("<>", Token_Class.NotEqualOp);
+            Operators.Add(":=", Token_Class.AssignmentOp);
             Operators.Add("+", Token_Class.PlusOp);
+            Operators.Add("-", Token_Class.MinusOp);
+            Operators.Add("/", Token_Class.DivideOp);
+            Operators.Add("*", Token_Class.MultiplyOp);
+            //Operators.Add(".", Token_Class.Dot);
             // TODO: Add Operators
         }
 
@@ -82,7 +113,7 @@ namespace Tiny_Language
             Token Tok = new Token();
             Tok.lex = Lex;
             //Is it a reserved word?
-
+            
             //Is it an identifier?
 
             //Is it a Constant?
@@ -95,16 +126,20 @@ namespace Tiny_Language
 
         bool isIdentifier(string lex)
         {
-            bool isValid = true;
+            bool isValid = false;
             // Check if the lex is an identifier or not.
-
+            var exp = new Regex(@"^[a-zA-Z][a-zA-Z0-9]*$", RegexOptions.Compiled);
+            if (exp.IsMatch(lex))
+                isValid = true;
             return isValid;
         }
         bool isConstant(string lex)
         {
-            bool isValid = true;
+            bool isValid = false;
             // Check if the lex is a constant (Number) or not.
-
+            var exp = new  Regex(@"^[0-9](.[0-9]+)*$",RegexOptions.Compiled);
+            if (exp.IsMatch(lex))
+                isValid = true;
             return isValid;
         }
     }
